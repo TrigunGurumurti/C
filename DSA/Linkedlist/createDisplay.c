3dad5f3f3a46848fe36c0f9c602138184926d16e
TrigunGurumurti <triguntamragouri@gmail.com>
2024-09-19 09:46:13 +0530

    createDisplay

---
+++ DSA/Linkedlist/createDisplay.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h> // for malloc function
+
+typedef struct node // self defined data type
+{
+    int data;
+    struct node *next; // data type -> struct node
+}node;                 // node is not created 
+
+int main()
+{
+    node *head= NULL;
+    node *newNode=NULL;
+    node *temp=NULL;
+    int choice=1,count=0;
+    while(choice)
+  {  
+    newNode=(node *)malloc(sizeof(node));
+    printf("Enter the data : ");
+    scanf("%d",&newNode->data);
+    newNode->next=NULL;
+    if(head==NULL)
+    {
+        head=temp=newNode;
+    }
+    else
+    {
+        temp->next=newNode;
+        temp=newNode;
+    }
+    printf("Do you want to continue : ");
+    scanf("%d",&choice);
+  } 
+
+  // Traverse 
+  temp=head;
+  while(temp!=NULL)
+  {
+    printf("%d -> ",temp->data);
+    temp=temp->next;
+    count++;
+  }
+    printf("\nNumber of nodes : %d",count);
+    return 0;
+}
